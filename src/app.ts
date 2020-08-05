5e2a9e59573f53b69cf0e249e0ae05e5902819c3
Maurizio Sodano <maurizio.sodano@soulsoftware.it>
2020-08-05 20:39:46 +0200

    added mongoose for persistence

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,46 @@
 import express, { Request, Response, NextFunction } from "express";
 import todoRoutes from "./routes/todos";
 import itemRoutes from "./routes/items";
+require('dotenv').config()
+//import * as cors from "cors";
+import connect from './mongo/connect';
 
-import {json} from "body-parser"
+import { json } from "body-parser"
+
+
 const app = express();
-app.use(json());
+/* //get router
+var router = express.Router();
 
+//options for cors midddleware
+const options:cors.CorsOptions = {
+  allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept", "X-Access-Token"],
+  credentials: true,
+  methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+  //origin: API_URL,
+  preflightContinue: false
+};
+
+//use cors middleware
+router.use(cors.default(options));
+
+//add your routes
+
+//enable pre-flight
+router.options("*", cors.default(options)) */
+
+
+app.use(json());
+//console.log(process.env.DB_HOST);
+
+const db = process.env.DB_HOST as string;
+connect({ db });
 app.use("/todos", todoRoutes);
 app.use("/items", itemRoutes);
 
-app.use((err:Error,req:Request, res:Response, next: NextFunction  )=>{
-  res.status(500).json({message:err.message})
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({ message: err.message })
 });
+
 
 app.listen(4000);
--- src/controllers/items.ts
+++ src/controllers/items.ts
@@ -1,50 +1,90 @@
 import { RequestHandler } from "express";
-import { Item,Note } from "../models/item";
+import { Item, Note } from "../models/item";
 import { v4 as uuidv4 } from "uuid";
+import { Schema, model } from "mongoose";
+
 const items: Item[] = [];
 
+//SCHEMA
+const itemsSchema = new Schema({
+    id: String,
+    title: String,
+    quantity: Number,
+    weight: Number,
+    insertion_date: String,
+    best_before_date: String
+});
+//MODEL
 
-export const createItem: RequestHandler = (req, res, next) => {
-    const item=req.body as Note;
-    const {title,quantity,weight,insertion_date,best_before_date} = item ;
+const ItemModel = model("Item", itemsSchema);
+
+export const createItem: RequestHandler = (req, res) => {
+    const item = req.body as Note;
+    const { title, quantity, weight, insertion_date, best_before_date } = item;
 
     const id = uuidv4();
-    
-    const newItem = new Item(id, title,quantity,weight,insertion_date,best_before_date);
-    items.push(newItem);
 
-    res.status(201).json({ message: "created the item.", createItem: newItem });
+    const newItem = new Item(id, title, quantity, weight, insertion_date, best_before_date);
+
+    const itemModel = new ItemModel({
+        title: newItem.title,
+        id: newItem.id,
+        quantity: newItem.quantity,
+        weight: newItem.weight,
+        insertion_date: newItem.insertion_date,
+        best_before_date: newItem.best_before_date,
+    });
+    itemModel.save((err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.status(201).json({ message: "created the item.", createItem: newItem });
+        }
+    });
+
+
 };
 
-export const getItems: RequestHandler = (req, res, next) => {
-    res.status(201).json({ items: items });
+export const getItems: RequestHandler = (req, res) => {
+    ItemModel.find({}, (err, freezerItems) => {
+        if (!err) {
+            res.status(201).send(freezerItems);
+        } else {
+            res.send(err);
+        }
+    });
+
 }
 
-export const updateItem:RequestHandler<{id:string}>=(req, res, next) => {
-    const itemId=req.params.id;
-
-    const updatedText=(req.body as {title:string}).title;
-    const itemIndex=items.findIndex(item=>item.id===itemId)
-
-    if (itemIndex<0){
-        throw new Error("could not find item!");
-    }
-    items[itemIndex]=new Item(itemId,updatedText);
-
-    res.status(201).json({message:"Updated",updateItem:items[itemIndex]});
+export const updateItem: RequestHandler<{ id: string }> = (req, res) => {
+    const itemId = req.params.id;
+    ItemModel.updateOne({
+        id: itemId
+    }, {
+        $set: req.body
+    },
+        (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send("Successfully updated note");
+            }
+        })
 }
 
 
-export const deleteItem:RequestHandler<{id:string}>=(req, res, next) => {
-    const itemId=req.params.id;
+export const deleteItem: RequestHandler<{ id: string }> = (req, res) => {
+    const itemId = req.params.id;
+    console.log("deleting id:" + itemId);
 
-   
-    const itemIndex=items.findIndex(item=>item.id===itemId)
-
-    if (itemIndex<0){
-        throw new Error("could not find item!");
-    }
-    items.splice(itemIndex,1)
-
-    res.status(201).json({message:"deleted"});
+    ItemModel.deleteOne({
+        id: itemId
+    },
+        (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send("Successfully deleted");
+            }
+        })
 }
